c0400352abda9cfb90f8b1044d66c2e335ecbb9a
jemicom <103623278+jemicom@users.noreply.github.com>
2023-03-19 00:36:32 +0900

    comment update

--- src/components/Home.js
+++ src/components/Home.js
@@ -40,7 +40,9 @@ const Home = () => {
             animate={{ opacity: 1 }}
             transition={{ delay: 1.5, duration: 1.5 }}
           >
-            <h2>Welcome to Pizza Joint</h2>
+           <h2>
+               Pizza React
+            </h2>
             <Link to="/base">
               
               <motion.button
